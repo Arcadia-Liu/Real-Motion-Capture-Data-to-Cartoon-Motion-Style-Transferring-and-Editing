9de1b98c1a5dace23855b7c7c9f2ef9bc3160400
Arcadia-Liu <fangzeliu511@gmail.com>
2024-05-12 23:04:23 -0400

    1. Update Frame Controller. Adjust environment frame rate to change the FPS of overall project. Adjust animation frame rate to change the motion updating frequency of character. Adjust play rate to change the playing speed of BVH files. 2. Add some notes for the BVH Driver Script. 3. Add two scripts to controll the hiding UI of Inspector 4. Modified the bonemap for CMU motion data

--- Assets/Scripts/1_BVHParser/BVHDriver.cs
+++ Assets/Scripts/1_BVHParser/BVHDriver.cs
@@ -4,76 +4,95 @@ using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts;
+using JetBrains.Annotations;
 
 // TODO: 调整frame
 // TODO: 目前只缩放了左腿骨骼的长度
 
 public class BVHDriver : MonoBehaviour
 {
+    /*************************************  public 变量  ****************************************/
     [Header("Loader settings")]
     [Tooltip("This is the target avatar for which the animation should be loaded. Bone names should be identical to those in the BVH file and unique. All bones should be initialized with zero rotations. This is usually the case for VRM avatars.")]
     public Animator targetAvatar;
 
     [Tooltip("This is the path to the file which describes Bonemaps between unity and bvh.")]
     public string bonemapPath = @"Assets/Scripts/0_BoneMaps/Bonemaps_CMU.txt";
+
     [Tooltip("This is the path to the BVH file that should be loaded. Bone offsets are currently being ignored by this loader.")]
     public string filename;
+
     [Tooltip("If the flag above is disabled, the frame rate given in the BVH file will be overridden by this value.")]
-    public float frameRate = 24.0f;
-    public float nearRatio = 30000f;
-    public GameObject modelMesh;
+    public float environmentFrameRate = 24.0f;
+    public float animationFrameRate = 24.0f;
+
+    [Tooltip("Define the start position of animation.")]
+    public GameObject startPos;
+
+    [SerializeField]
+    private bool ifUsingCustomizedRate = true;
+    [ConditionalHide(nameof(ifUsingCustomizedRate), false), SerializeField]
+    private float playRate = 50.0f;
 
     [Serializable]
-    public struct BoneMap
+    public struct BoneMap       // the corresponding bones between unity and bvh
     {
         public string bvh_name;
         public HumanBodyBones humanoid_bone;
     }
-    public BoneMap[] bonemaps; // the corresponding bones between unity and bvh
-    public GameObject startPos;
-    public Camera currentCamera;
+    public BoneMap[] bonemaps; 
 
+
+    /***********************************  private 变量  ****************************************/
     private BVHParser bp = null;
-    private Animator anim;
-
-    // This function doesn't call any Unity API functions and should be safe to call from another thread
+    private float frameRate;
+    private float originalFrameRate;
+    private float frameRatio;
+    // 该函数不调用任何 Unity API 函数，因此可以安全地从另一个线程调用
     public void parseFile()
     {
         string bvhData = File.ReadAllText(filename);
-        bp = new BVHParser(bvhData);    
-        frameRate = 1f / bp.frameTime;
+        bp = new BVHParser(bvhData);
+        // frameRate = 1f / bp.frameTime;
+        originalFrameRate = 1f / bp.frameTime;
+        frameRate = environmentFrameRate;
     }
 
+    private Animator anim;
+    private Camera currentCamera;
     private Dictionary<string, Quaternion> bvhT;
     private Dictionary<string, Vector3> bvhOffset;
     private Dictionary<string, string> bvhHireachy;
     private Dictionary<HumanBodyBones, Quaternion> unityT;
-
     private int frameIdx;
     private float scaleRatio = 0.0f;
+    private float nearRatio = 0.0f;
 
     private void Start()
     {
         
         // set mapping between bvh_name and humanBodyBones
-        // 调用BoneReader.Read，解析BVH文件
-        BonemapReader.Read(bonemapPath);
-        // 设置 bvh_name 和 humanBodyBones 之间的映射关系
-        bonemaps = BonemapReader.bonemaps;
-        
+        BonemapReader.Read(bonemapPath);        // 调用BoneReader.Read，解析BVH文件
+        bonemaps = BonemapReader.bonemaps;      // 设置 bvh_name 和 humanBodyBones 之间的映射关系
         parseFile();
         Application.targetFrameRate = (Int16)frameRate;
+        if(ifUsingCustomizedRate == false)
+        {
+            Time.fixedDeltaTime = 1 / originalFrameRate;
+        }
+        else
+        {
+            Time.fixedDeltaTime = 1 / playRate;
+        }
 
-        // 获取bvh文件第0帧位置的BoneData，即bvh骨骼的Tpose信息
-        bvhT = bp.getKeyFrame(0);
-        bvhOffset = bp.getOffset(1.0f);
-        // 获得bvh文件的骨架结构信息，包含骨骼名称和骨骼节点的父子关系
-        bvhHireachy = bp.getHierachy();
-        // 获取目标角色模型的Animator组件，用于后续对角色动画的控制
-        anim = targetAvatar.GetComponent<Animator>();
+        bvhT = bp.getKeyFrame(0);       // 获取bvh文件第0帧位置的BoneData，即bvh骨骼的Tpose信息
+        bvhOffset = bp.getOffset(1.0f); 
+        bvhHireachy = bp.getHierachy(); // 获得bvh文件的骨架结构信息，包含骨骼名称和骨骼节点的父子关系
+        anim = targetAvatar.GetComponent<Animator>();   // 获取目标角色模型的Animator组件，用于后续对角色动画的控制
         unityT = new Dictionary<HumanBodyBones, Quaternion>();
-        // 遍历骨骼映射数据
-        foreach (BoneMap bm in bonemaps)
+        frameRatio = originalFrameRate / animationFrameRate;
+        print(frameRatio);
+        foreach (BoneMap bm in bonemaps)    // 遍历骨骼映射数据
         {
             // 将Tpose下，unity对象内置骨骼的名称和相对世界坐标系的旋转信息记录在unityT中
             unityT.Add(bm.humanoid_bone, anim.GetBoneTransform(bm.humanoid_bone).rotation);
@@ -90,45 +109,31 @@ public class BVHDriver : MonoBehaviour
                 bvh_leftleg = bvh_leftleg + (float)Math.Sqrt(bvhOffset[bm.bvh_name].sqrMagnitude);
             }
         }
-        // 计算骨骼缩放
-        scaleRatio = unity_leftleg / bvh_leftleg;
+        scaleRatio = unity_leftleg / bvh_leftleg;   // 得到骨骼缩放比例
         frameIdx = 1;
-
-        
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        // 得到当前帧骨骼数据boneData
-        Dictionary<string, Quaternion> currFrame = bp.getKeyFrame(frameIdx);    // frameIdx 2871
+        /**********************   获取当前帧和上一临近帧的骨骼数据 boneData   **********************/
+        Dictionary<string, Quaternion> currFrame = bp.getKeyFrame(frameIdx);
         Dictionary<string, Quaternion> lastFrame = bp.getKeyFrame(frameIdx - 1);
-        
-
-        // 根据bvh中骨骼关节点的local rotation，更新unity中的animation骨骼相对世界坐标系的旋转rotation
-        BVHLocalRotationToUnityRotation(currFrame, bonemaps, bvhT, unityT);
+        if (frameIdx % (Int16)frameRatio == 1)
+        {
+            /****************************   更新骨骼关节节点旋转   *********************************/
+            // 根据bvh中骨骼关节点的local rotation，更新unity中的animation骨骼相对世界坐标系的旋转rotation
+            BVHLocalRotationToUnityRotation(currFrame, bonemaps, bvhT, unityT);  
+        }
+        /**********************   将BVH中的人物模型位移转换为unity 2D 运动规范   ******************/
         Dictionary<string, Vector3> currBVHPos = getBVHPos(currFrame, bvhHireachy, bvhOffset);
         Dictionary<string, Vector3> lastBVHPos = getBVHPos(lastFrame, bvhHireachy, bvhOffset);
-        // BVHPosToBVHMove(currBVHPos);
-
-
-
-        // 将BVH中的物体位移转换为unity 2D 运动规范
         BVHPosToUnityMovement(currBVHPos, lastBVHPos, frameIdx);
 
-
-        // 动画帧累加和循环播放
-        if (frameIdx < bp.frames - 1)
-        {
-            frameIdx++;
-        }
-        else
-        {
-            frameIdx = 1;
-        }
-
+        /*****************************   动画播放与循环   **********************************/
+        frameIdx = (frameIdx < bp.frames - 1) ? frameIdx + 1 : 1;
     }
 
-    // 返回各个关节节点在世界空间中的位置
+    // 返回在currFrame帧位置，各个关节节点在世界空间中的坐标
     private Dictionary<string, Vector3> getBVHPos(Dictionary<string, Quaternion> currFrame, 
         Dictionary<string, string> bvhHireachy, Dictionary<string, Vector3> bvhOffset)
     {
@@ -154,18 +159,29 @@ public class BVHDriver : MonoBehaviour
         return bvhPos;  
     }
 
+    /********************************************************
+     * BVHPosToUnityMovement：
+     * 通过当前帧和上一帧中各个关节节点在世界空间坐标中的坐标位置，驱动unity人形动画移动
+     * 
+     * moveDirection:   2D动作运动目标方向
+     * { Forward2D: 2D角色前后方向，
+     *   Up2D: 2D角色上下方向，
+     *   Near2D: 2D角色z轴深度方向}
+     * characterDirection:  人物模型自身方向定义
+     * currVelocity:    根节点的平均位移速度
+     * ******************************************************************/
     private void BVHPosToUnityMovement(Dictionary<string, Vector3> currBVHPos, Dictionary<string, Vector3> lastBVHPos, int frameIndex)
     {
-        // moveDirection:{ Forward2D: 2D角色前后方向，Up2D: 2D角色上下方向，Near2D: 2D角色z轴深度方向}
+        /************************    准备基础的速度和方向参数    ***************************/
         Dictionary<string, Vector3> moveDirection = get2DWorldDirection();
-        // 得到character自身的三维方向
         Dictionary<string, Vector3> characterDirection = get3DCharacterDirection(); 
-        // 计算根节点的平均位移速度
         Vector3 currVelocity = getAverageVelocity(currBVHPos, lastBVHPos);
         // 将速度投影到character自身的三维方向
         Vector3 forwardVelocity = Vector3.Project(currVelocity, characterDirection["Forward3D"]);
         Vector3 upVelocity = Vector3.Project(currVelocity, characterDirection["Up3D"]);
         Vector3 nearVelocity = Vector3.Project(currVelocity, characterDirection["Near3D"]);
+        
+        /************************    更新根节点在x和y方向的位移    ***************************/
         // Debug: 绘制当前位移速度方向
         Debug.DrawRay(anim.GetBoneTransform(HumanBodyBones.Hips).position, forwardVelocity * 1000f, Color.red);
         Debug.DrawRay(anim.GetBoneTransform(HumanBodyBones.Hips).position, upVelocity * 1000f, Color.blue);
@@ -174,6 +190,7 @@ public class BVHDriver : MonoBehaviour
         anim.GetBoneTransform(HumanBodyBones.Hips).position += Vector3.Dot(forwardVelocity.normalized, moveDirection["Forward2D"].normalized) * forwardVelocity.magnitude * moveDirection["Forward2D"] * bp.frameTime;
         anim.GetBoneTransform(HumanBodyBones.Hips).position += Vector3.Dot(upVelocity.normalized, moveDirection["Up2D"].normalized) * upVelocity.magnitude * moveDirection["Up2D"] * bp.frameTime;
 
+        /************************    更新人物模型在z轴向的缩放    ***************************/
         // 使用投影Near2D方向的方法更新深度影响的scale
         // 用相机矩阵计算nearRatio，然后调整加减正负的大小问题
         Vector3 characterHip = anim.GetBoneTransform(HumanBodyBones.Hips).transform.position;
@@ -184,7 +201,7 @@ public class BVHDriver : MonoBehaviour
         Vector3 newScale = new Vector3(gameObject.transform.localScale.x * nearScale, gameObject.transform.localScale.y * nearScale, gameObject.transform.localScale.z * nearScale);
         gameObject.transform.localScale += newScale * scaleSpeed;
 
-        // 循环播放动画的需要
+        /************************    控制动画循环播放    ***********************************/
         if (frameIndex == 1)
         {
             anim.GetBoneTransform(HumanBodyBones.Hips).position = startPos.transform.position;
@@ -192,6 +209,10 @@ public class BVHDriver : MonoBehaviour
         }
     }
 
+
+
+    /************************   控制z轴缩放 相关的函数   *************************************/
+    // 得到人物模型与当前相机的绝对距离
     private float Camera_GetVerticalDistance_Character(Vector3 characterHip)
     {
         currentCamera = UnityEngine.Camera.main;
@@ -200,19 +221,8 @@ public class BVHDriver : MonoBehaviour
         return distance;
     }
 
-    private void BVHPosToBVHMove(Dictionary<string, Vector3> bvhPos)
-    {
-        // 更新角色位置属性，将BVH文件中根骨骼的位置信息（经过缩放）赋值给Unity模型中对应骨骼的位置属性
-        anim.GetBoneTransform(HumanBodyBones.Hips).position = bvhPos[bp.root.name] * scaleRatio;
 
-        // draw bvh skeleton
-        foreach (string bname in bvhHireachy.Keys)
-        {
-            Color color = new Color(1.0f, 0.0f, 0.0f);
-            Debug.DrawLine(bvhPos[bname] * scaleRatio, bvhPos[bvhHireachy[bname]] * scaleRatio, color);
-        }
-    }
-
+    /*********************   更新骨骼关节节点旋转和位置更新 相关的函数   *************************/
     // 根据bvh中骨骼关节点的local rotation，更新unity中的animation骨骼相对世界坐标系的旋转rotation
     private void BVHLocalRotationToUnityRotation(Dictionary<string, Quaternion> currFrame, BoneMap[] bonemaps, 
         Dictionary<string, Quaternion> bvhT, Dictionary<HumanBodyBones, Quaternion> unityT)
@@ -226,6 +236,7 @@ public class BVHDriver : MonoBehaviour
         }
     }
 
+    /************************   更新人物模型位移和朝向 相关的函数   ******************************/
     private Dictionary<string, Vector3> get2DWorldDirection()
     {
         currentCamera = UnityEngine.Camera.main;
@@ -259,5 +270,22 @@ public class BVHDriver : MonoBehaviour
     {
         Vector3 averageVelocity = (currBVHPos[bp.root.name] - lastBVHPos[bp.root.name]) * scaleRatio / bp.frameTime;
         return averageVelocity;
+    }
+
+
+
+
+    /******************************   原BVH Parser的动作驱动   *************************************/
+    private void BVHPosToBVHMove(Dictionary<string, Vector3> bvhPos)
+    {
+        // 更新角色位置属性，将BVH文件中根骨骼的位置信息（经过缩放）赋值给Unity模型中对应骨骼的位置属性
+        anim.GetBoneTransform(HumanBodyBones.Hips).position = bvhPos[bp.root.name] * scaleRatio;
+
+        // draw bvh skeleton
+        foreach (string bname in bvhHireachy.Keys)
+        {
+            Color color = new Color(1.0f, 0.0f, 0.0f);
+            Debug.DrawLine(bvhPos[bname] * scaleRatio, bvhPos[bvhHireachy[bname]] * scaleRatio, color);
+        }
     }
 }
